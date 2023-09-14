33b681ffbfef5a0752349d31e6fed8563cf553ed
babayaga47 <114303509+babayaga47@users.noreply.github.com>
2023-09-14 20:26:18 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,131 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+var userClickedPattern = [];
+
+
+var started = false;
+
+var level = 0;
+
+$(document).keypress(function() {
+
+  if (started === false) {
+
+    $("#level-title").text("level " + level);
+    nextSequence()
+    started = true;
+
+  }
+
+
+});
+
+
+
+$(".btn").click(function() {
+
+  var userChosenColor = $(this).attr("id");
+
+
+  userClickedPattern.push(userChosenColor);
+
+  playSound(userChosenColor);
+
+  animatePress(this.id);
+
+  checkAnswer(userClickedPattern.length - 1);
+
+});
+
+
+function startOver() {
+
+ $("body").keypress(function(){
+
+  level = 0;
+  gamePattern = [];
+
+  started = false ;
+
+ });
+
+}
+
+
+
+function checkAnswer(currentLevel) {
+
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+
+
+    if (gamePattern.length === userClickedPattern.length) {
+
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+
+    }
+  } else {
+    console.log("wrong");
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+
+    setTimeout(function() {
+
+      $("body").removeClass("game-over");
+
+      $("h1").text("Game Over, Press Any Key To Restart")
+    }, 200);
+
+    startOver();
+  }
+}
+
+
+
+
+
+function nextSequence() {
+  userClickedPattern = [];
+
+  level++;
+
+  $("#level-title").text("level " + level);
+
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+
+  playSound(randomChosenColor);
+
+};
+
+
+function animatePress(currentColor) {
+  var animate = $("#" + currentColor);
+  animate.addClass("pressed");
+  setTimeout(function() {
+    animate.removeClass("pressed")
+  }, 100);
+
+}
+
+
+
+
+
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+
+}
